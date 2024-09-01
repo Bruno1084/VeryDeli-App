fdb6e01e0b05c23c2b21c81f916f532490d2bf2f
Octavio Riccardo <octariccardo@gmail.com>
2024-09-01 18:33:00 -0300

    creando funcion CheckEmail
    Con esta funcion podemos verificar si el email se encuentra en la base de datos , para poder saber si el email existe , cuando un usuario quiera registrar una cuenta

--- database/validations/register.php
+++ database/validations/register.php
@@ -1,18 +1,17 @@
 <?php
 
 
-function getAllComentarios ($idPublicacion) {
-  require '../database/conection.php';
+    function CheckEmail($Email) {
+    require '../database/conection.php';
 
-  $sql = "SELECT * FROM usuarios WHERE idUsuarios = $idPublicacion";
-  $resultado = $conexion->$query($sql);
+    $sql = "SELECT usuario_correo FROM usuarios WHERE usuario_correo=$Email";
+    $resultado = $conexion->query($sql);
 
-  if ($resultado->num_rows > 0) {
-    $resultado = $resultado->fetch_all(MYSQLI_ASSOC);
-  } else {
-    $resultado = [];
-  };
+    if ($resultado->num_rows > 0) {
+        $resultado = $resultado->fetch_all(MYSQLI_ASSOC);
+        return true;
+    } else {
+        return false;
+    };
 
-  return $resultado;
-}
-?>
+    }
