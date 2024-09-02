1439bd637c44af501578b30a0722a0c56acd6a3a
Leandro998 <leandroaguero83@gmail.com>
2024-09-01 23:50:57 -0300

    add funcion util -> verificar campos obligatorios

--- utils/registerUser.php
+++ utils/registerUser.php
@@ -2,6 +2,17 @@
 
 function guardarUsuario($nombre, $apellido, $correo, $localidad, $usuario, $contrasenia){
   require_once("../database/conection.php");
+  require_once("../utils/functions/limpiarCadena.php");
+
+  //Almacena los datos
+
+  $nombre = $_POST['nombre'];
+  $apellido = $_POST['apellido'];
+  $correo = $_POST['correo'];
+  $localidad = $_POST['localidad'];
+  $usuario = $_POST['usuario'];
+  $contrasenia = $_POST['contraseña'];
+  
   $contrasenia = password_hash($contrasenia, PASSWORD_BCRYPT, ["cost"=>10]);
   $sql = $conexion->prepare("INSERT INTO usuarios (usuario_nombre, usuario_apellido, usuario_correo, usuario_localidad, usuario_usuario, usuario_contraseña) VALUES (:nombre, :apellido, :correo, :localidad, :usuario, :contrasenia)");
   // Vincular los parámetros
