43833e9eac4a9384ed2d75062d7a97e7e4c31c73
Leandro998 <leandroaguero83@gmail.com>
2024-09-01 22:42:16 -0300

    Add funciones utiles

---
+++ utils/functions/verificarDatos.php
@@ -0,0 +1,9 @@
+<?php
+    function verificarDatos($filtro, $dato){
+        if(preg_match("/^".$filtro."$/", $dato)){
+            return false;   // Si la cadena no contiene errores retorna false
+        }else{
+            return true;    // Si la cadena contiene errores retorna true
+        }
+    }
+?>
