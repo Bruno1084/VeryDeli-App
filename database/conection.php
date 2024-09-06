451a15f6d316eefc3921a7bcee3236e9edf0194b
Leandro998 <leandroaguero83@gmail.com>
2024-09-06 00:13:46 -0300

    cree una funcion que retorna una conexion, evita problemas al manejar la conexion en varios archivos

--- database/conection.php
+++ database/conection.php
@@ -1,11 +1,12 @@
 <?php
-
-  $conexion = mysqli_connect('localhost', 'root', 'Toon', 'verydeli');
-
-  if ($conexion) {
-    echo 'La conexión a la base de datos fue exitosa';
-  } else {
-    echo 'Error al conectar la base de datos';
-  };
+  function conectarBD(){
+    $conexion = mysqli_connect('localhost', 'root', '', 'verydeli');
+    if ($conexion) {
+      echo 'La conexión a la base de datos fue exitosa';
+      return $conexion;
+    } else {
+      echo 'Error al conectar la base de datos';
+    };
+  }
   
 ?>
