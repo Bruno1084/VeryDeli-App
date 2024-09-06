a0711e98ed71ac579bc5432580c50fbcd5eba2fb
Leandro998 <leandroaguero83@gmail.com>
2024-09-06 00:17:14 -0300

    add logica registrar transportista

--- utils/registrarTransportista.php
+++ utils/registrarTransportista.php
@@ -1,9 +1,8 @@
 <?php
     function registrarTransportista($usId){
-        require_once("../database/conection.php");
-        $stmt = $conexion->prepare("INSERT INTO transportistas transportista_id VALUES ?");
+        $conexion = conectarBD();
+        $stmt = $conexion->prepare("INSERT INTO transportistas (transportista_id) VALUES (?)");
         $stmt->bind_param("s", $usId);
-
         return $stmt->execute();
     }
 ?>
