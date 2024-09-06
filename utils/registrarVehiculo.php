45e5de87811beca01a742c218e778ee6c0e5934f
Leandro998 <leandroaguero83@gmail.com>
2024-09-06 00:16:31 -0300

    add logica para registrar un vehiculo

--- utils/registrarVehiculo.php
+++ utils/registrarVehiculo.php
@@ -1,7 +1,7 @@
 <?php
     
     function guardarVehiculo($usId, $tipoVehiculo, $patente, $pesoSoportado, $volumenSoportado){
-        require_once("../database/conection.php");
+        $conexion = conectarBD();
         $stmt = $conexion->prepare("INSERT INTO vehiculos (vehiculo_patente, vehiculo_tipoVehiculo, vehiculo_pesoSoportado, vehiculo_volumenSoportado, transportista_id) VALUES (?, ?, ?, ?, ?)");
         $stmt->bind_param("sssss", $patente, $tipoVehiculo, $pesoSoportado, $volumenSoportado, $usId);
     
