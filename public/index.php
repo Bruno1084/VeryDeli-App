a4541bde11ee07659749c7c7b1e8fbb3d01311e7
Lean998 <leandroaguero83@gmail.com>
2024-10-03 13:00:41 -0300

    add validaciones para todos los campos excepto para el input imagenes

--- public/index.php
+++ public/index.php
@@ -39,16 +39,22 @@
           <button type="button" class="btn-close bg-white" data-bs-dismiss="modal" aria-label="Cerrar"></button>
         </div>
         <div class="modal-body">
-          <form action="#" class="form-publicacion" method="post" enctype="multipart/form-data" id="formPublicacion" autocomplete="off" onsubmit="return validarPublicacion()">
+          <form action="#" class="form-publicacion needs-validation" novalidate method="post" enctype="multipart/form-data" id="formPublicacion" autocomplete="off" onsubmit="return validarPublicacion()">
             <div class="row">
               <div class="col-3 mb-3">
                 <input type="text" class="form-control " id="publicacion-titulo" name="titulo" placeholder="Titulo" required>
+                <div class="invalid-feedback">
+                  Es necesario ingresar el titulo!
+                </div>
               </div>
             </div>
 
             <div class="row">
               <div class="col-12 mb-3">
                 <textarea style="height: 120px; max-height:120px" class="form-control" id="publicacion-descripcion" name="descripcion" placeholder="Descripcion" required></textarea>
+                <div class="invalid-feedback">
+                  Ingresa una descripción para tú solicitud!
+                </div>
               </div>
             </div>
 
@@ -56,27 +62,41 @@
             <div class="row">
               <div class="col-3">
                 <input type="text" class="form-control mb-3" id="publicacion-volumen" name="volumen" required placeholder="Volumen">
+                <div class="invalid-feedback">
+                  Ingrese el volumen aproximado de la carga!
+                </div>
               </div>
               <div class="col-3">
                 <input type="text" class="form-control" id="publicacion-peso" name="peso" required placeholder="Peso">
+                <div class="invalid-feedback">
+                  Ingrese el peso aproximado de la carga!
+                </div>
               </div>
             </div>
 
             <div class="row mb-3">
               <div class="col-3">
                 <input type="text" class="form-control mb-3" id="publicacion-origen" name="origen" required placeholder="Origen">
-              
                 <input type="text" class="form-control" id="publicacion-destino" name="destino" required placeholder="Destino">
+                <div class="invalid-feedback">
+                  Es necesario ingresar los datos para el envio!
+                </div>
               </div>
             </div>
 
             <div class="row mb-2">
               <div class="col-3 mb-3">
                 <input type="text" class="form-control" id="publicacion-recibe" name="recibe" required placeholder="Encargado de Recibir">
+                <div class="invalid-feedback">
+                  Ingrese el nombre de la persona que recibira la carga!
+                </div>
               </div>
 
               <div class="col-3">
                 <input type="tel" class="form-control" id="publicacion-contacto" name="contacto" required placeholder="Telefono de Contacto">
+                <div class="invalid-feedback">
+                  Ingrese un telefono de contacto!
+                </div>
               </div>
             </div>
 
@@ -86,7 +106,7 @@
                 <h2 id="addPhoto">Imagenes ➕</h2>
                 <div id="photos"></div>
                 <input type="file" class="addNewPhoto" accept="image/png, image/jpeg, image/jpg" name="addNewPhoto-0[]" id="addNewPhoto" multiple/>
-              </div>
+                </div>
             </div>
           </form>
         </div>
@@ -105,7 +125,25 @@
     function validarPublicacion(){
       return true;  // Permite el envío del formulario
     }
+  
+  (() => {
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
   </script>
   <?php require_once("../components/JS.php"); ?>
 </body>
