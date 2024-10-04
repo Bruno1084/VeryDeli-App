208600d813e5da8b96dfb3400de1c74c77896ba5
Octavio Riccardo <octariccardo@gmail.com>
2024-10-04 19:32:29 -0300

    .

--- components/card-publicacion.php
+++ components/card-publicacion.php
@@ -1,8 +1,27 @@
-<div class="card" style="width: 18rem;">
+<!-- <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</div>
+</div> -->
+
+
+<div class="contenedor-fluid">
+        <div class="row border-bottom border-2" name="publicacion_D" id="publicacion-N_AD">
+          <div class="col-3 publicacion_cabecera">
+                <div class="user_photo">
+                    <img class="img u_photo w-50 h-50" src="../assets/Logo.png" alt="user">
+                </div>
+          </div>
+          <div class="col-4">
+                <div class=""><p>Bruno Sosa</p></div>
+                <div class=""><p>San Luis Argentina</p></div>
+          </div>
+          <div class="col-5 publication_date">
+                <div class=""><p>12:30</p></div>
+                <div class=""><p>12703/20</p></div>
+          </div>
+        </div>              
+    </div>
