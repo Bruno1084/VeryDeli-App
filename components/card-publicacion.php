33eb2c5ef10abbcca46a4230a52ccb93dfcca2a8
Octavio Riccardo <octariccardo@gmail.com>
2024-10-07 02:46:36 -0300

    terminando la tarjeta de la publicacion

--- components/card-publicacion.php
+++ components/card-publicacion.php
@@ -15,32 +15,57 @@
                     <img class="img u_photo w-50 h-50" src="../assets/Logo.png" alt="user">
                 </div>
           </div>
-          <div class="col-4 mt-1">
-                <div><p>Bruno Sosa</p></div>
-                <div><p>San Luis Argentina</p></div>
+          <div class="col-4  mt-1">
+                <div><h5>Bruno Sosa</h5></div>
+                <div><h5>San Luis Argentina</h5></div>
           </div>
           <div class="col-5 mt-1">
-                <div><p>12:30</p></div>
-                <div><p>12703/20</p></div>
+                <div><h5>12:30</h5></div>
+                <div><h5>12703/20</h5></div>
           </div>
         </div>
         
-        <div class="row">
+        <div class="row m-3">
             <div class="col-12">
-                <div class="">
+                <div class="d-flex justify-content-start">
                     <h5>Detalles del producto:</h5>
                 </div>
             </div>
             <div class="col-12">
-                <div class="">
+                <div class="d-flex justify-content-start">
                     <p>Dicta a libero esse fugiat eius impedit, doloribus quaerat sapiente reprehenderit quo iusto molestiae recusandae aspernatur ut. Itaque hic distinctio, voluptas tempora quisquam id veritatis eius?</p>
                 </div>
             </div>
         </div>
-
+        <div class="row m-3">
+            <div class="col-4 d-flex justify-content-start">
+                <div class="">
+                    <h5>Peso:</h5>
+                    <p>85kg</p>
+                </div>
+            </div>
+        
+            <div class="col-4 d-flex justify-content-start">
+                <div class="">
+                    <h5>Origen:</h5>
+                    <p>Maipu 245</p>
+                </div>
+            </div>
+                <div class="col-4 d-flex justify-content-start">
+                <div class="">
+                    <h5>Destino:</h5>
+                     <p>Lavalle 1002</p>
+                </div>
+            </div>
+        </div>
+       <div class="row m-3">
+            <div class="col-12 d-flex justify-content-start">
+                <button type="button" class="btn btn-gris btn-lg">Postularse</button>
+            </div>
+        </div>
         <div class="row">
             <div class="col-12">
-                <div class="">
+                <div class="border border-dark-3">
                    <img class="img u_photo w-50 h-50" src="../assets/profile.svg" alt="user">
                 </div>
             </div>
