aeb13e3093bbf7b30ae0dc0673e24335c060f5fe
Lean998 <leandroaguero83@gmail.com>
2024-10-07 19:34:10 -0300

    add validaciones formPublicacion

--- utils/ControlFormPublicacion.php
+++ utils/ControlFormPublicacion.php
@@ -8,9 +8,60 @@
         return substr(str_shuffle($txt),0, $tam);
     }
     
-    if(!empty($_POST["photosId"])){
-        $fotos=$_POST["photosId"];
+    if(!empty($_POST["photosId"]) && isset($_POST['enviado'])){
+        session_start();
+        require_once($_SERVER['DOCUMENT-ROOT'] . "/utils/functions/manejaError.php");
+        require_once($_SERVER['DOCUMENT-ROOT'] . "/database/conection.php");
+        $fotos = $_POST["photosId"];
+        $titulo = $_POST['titulo'];
+        $descripcion = $_POST['descripcion'];
+        $volumen = $_POST['volumen'];
+        $peso = $_POST['peso'];
+        $origen = $_POST['origen'];
+        $destino = $_POST['destino'];
+        $recibe = $_POST['recibe'];
+        $telContacto = $_POST['contacto'];
         $formatSuportPhoto=["image/png","image/jpeg","image/jpg"];
+        $usuarioAutor = $_SESSION['id'];
+        $db = new DB();
+        $conexion = $db->getConnection();
+        
+        if(!$_SESSION['esResponsable']){
+          $stmtCantPublicaciones = $conexion->prepare("SELECT * FROM publicaciones WHERE usuario_autor = ? AND publicacion_esActivo = true");
+          $stmtCantPublicaciones->bindParam(1, $usuarioAutor, PDO::PARAM_INT);
+          if($stmtCantPublicaciones->execute()){
+            if($stmtCantPublicaciones->rowCount() > 3){
+              manejarError('Limite excedido', "Has excedido el limite de publicaciones activas.");
+              $stmtCantPublicaciones = null;
+              exit;
+            }
+          } else {
+            $stmtCantPublicaciones = null;
+            manejarError("Error Inesperado", "Ocurrio un error al momento de validar su cantidad de publicaciones");
+            exit;
+          }
+        }
+        
+        $stmtPublicacion = $conexion->prepare("INSERT INTO publicaciones (publicacion_titulo, publicacion_descr, publicacion_peso, publicacion_volumen, publicacion_origen, publicacion_destino, publicacion_nombreRecibe, publicacion_telefono, usuario_autor, publicacion_esActivo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, true)");
+        $stmt->bindParam(1, $titulo, PDO::PARAM_STR);
+        $stmt->bindParam(2, $descripcion, PDO::PARAM_STR);
+        $stmt->bindParam(3, $peso);
+        $stmt->bindParam(4, $volumen);
+        $stmt->bindParam(5, $origen, PDO::PARAM_STR);
+        $stmt->bindParam(6, $destino, PDO::PARAM_STR);
+        $stmt->bindParam(7, $recibe, PDO::PARAM_STR);
+        $stmt->bindParam(8, $telContacto, PDO::PARAM_STR);
+        $stmt->bindParam(9, $usuarioAutor, PDO::PARAM_INT);
+        
+        if($stmtPublicacion->execute()){
+          $newPubli = $stmtPublicacion->fetch();
+          $idPub = $newPubli['publicacion_id'];
+        } else {
+          $stmtPublicacion = null;
+          manejarError("Error Inesperado", "Ocurrio un error al momento de subir tu publicacion, intente de nuevo más tarde");
+          exit;
+        }
+
         for($i=0;$i<count($fotos);$i+=2){
             if(!in_array($fotos[$i+1],$formatSuportPhoto)){
                 $data["error"]="Error, Se encontro un tipo no valido";
@@ -35,7 +86,7 @@
                 }
             }
             if(empty($data)){
-                $response=$dbImgBB->guardarImagenesDB($urlFotos,1);
+                $response=$dbImgBB->guardarImagenesDB($urlFotos,$idPub);
                 if(!$response){
                     $data["error"]="Error al guardar las fotos en la Base de Datos";
                     require_once("../utils/getImagen.php");
