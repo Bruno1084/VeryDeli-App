0693176e7e3a023c31b7942838b0d81657613e3a
Octavio Riccardo <octariccardo@gmail.com>
2024-10-07 01:48:56 -0300

    modelando la tarjeta de las publicaciones en el menu de inicio

--- components/card-publicacion.php
+++ components/card-publicacion.php
@@ -8,20 +8,52 @@
 </div> -->
 
 
-<div class="contenedor-fluid">
-        <div class="row border-bottom border-2" name="publicacion_D" id="publicacion-N_AD">
-          <div class="col-3 publicacion_cabecera">
-                <div class="user_photo">
+<div class="contenedor-fluid border border-3">
+        <div class="row" name="publicacion_D" id="publicacion-N_AD">
+          <div class="col-3 mt-1">
+                <div>
                     <img class="img u_photo w-50 h-50" src="../assets/Logo.png" alt="user">
                 </div>
           </div>
-          <div class="col-4">
-                <div class="pt-0"><p>Bruno Sosa</p></div>
-                <div class="pt-0"><p>San Luis Argentina</p></div>
+          <div class="col-4 mt-1">
+                <div class=""><p>Bruno Sosa</p></div>
+                <div class=""><p>San Luis Argentina</p></div>
           </div>
-          <div class="col-5 publication_date">
+          <div class="col-5 mt-1">
                 <div class=""><p>12:30</p></div>
                 <div class=""><p>12703/20</p></div>
           </div>
-        </div>              
+        </div>
+        
+        <div class="row">
+            <div class="col-12">
+                <div class="">
+                    <h5>Detalles del producto:</h5>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="">
+                    <p>Dicta a libero esse fugiat eius impedit, doloribus quaerat sapiente reprehenderit quo iusto molestiae recusandae aspernatur ut. Itaque hic distinctio, voluptas tempora quisquam id veritatis eius?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="">
+                   <img class="img u_photo w-50 h-50" src="" alt="user">
+                </div>
+            </div>
+            
+        </div>
+
+
     </div>
+
+
+
+
+
+
+
+    
