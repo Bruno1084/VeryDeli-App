6daf117e38fc9255deb8d139e124d4aa62d76fe8
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-10-12 13:57:22 -0300

    update user
    ahora también chequea el email, y el pass junto con el email

--- utils/user.php
+++ utils/user.php
@@ -18,6 +18,23 @@
         
         return $user;
     }
+    public static function emailExist($correo){
+        //Verifica si el nombre de usuario está registrado
+        $db=new DB();
+        $conexion=$db->getConnection();
+        
+        $sql="SELECT usuario_correo FROM usuarios WHERE usuario_correo = ?";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindValue(1, $correo, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $user=$stmt->fetch();
+        
+        $stmt=null;
+        $conexion=null;
+        
+        return $user;
+    }
 
     public static function userPassExist($usuario,$contrasenia){
         //Verifica que la contraseña corresponda al usuario
@@ -47,6 +64,47 @@
         }
         $stmt=null;
         $conexion=null;
+        return false;
+    }
+    public static function emailUserPassExist($correo,$contrasenia){
+        //Verifica que la contraseña corresponda al usuario
+        $db=new DB();
+        $conexion=$db->getConnection();
+        
+        $sql="SELECT usuario_contraseña FROM usuarios WHERE usuario_correo = ?";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindValue(1, $correo, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $pass=$stmt->fetch();
+        $stmt=null;
+        $conexion=null;
+        if($pass!=false){
+            if(password_verify($contrasenia, $pass[0])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function setEmailUserPass($correo,$contrasenia){
+        $db=new DB();
+        $conexion=$db->getConnection();
+        $hashPass=password_hash($contrasenia, PASSWORD_BCRYPT, ["cost"=>10]);
+        $sql="UPDATE usuarios SET usuario_contraseña = ? WHERE usuario_correo = ?";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindValue(1, $hashPass, PDO::PARAM_STR);
+        $stmt->bindValue(2, $correo, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $pass=$stmt->fetch();
+        $stmt=null;
+        $conexion=null;
+        if($pass!=false){
+            if(password_verify($contrasenia, $pass[0])){
+                return true;
+            }
+        }
         return false;
     }
 
