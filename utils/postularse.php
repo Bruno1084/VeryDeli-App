dd3d37137b5163a1630e6ff37e514733237c97b4
Lean998 <leandroaguero83@gmail.com>
2024-10-14 12:05:48 -0300

    controlar que un usuario no se postule a su propia publicacion y manejo de fecha con el tipo date de php

--- utils/postularse.php
+++ utils/postularse.php
@@ -15,6 +15,7 @@
     if($stmtTransportista->rowCount() == 0){
       manejarError('false', 'Usuario Transportista', 'Para postularse a una publicacion usted debe ser transportista');
       $stmtTransportista = null;
+      $conexion = null;
       exit;
     } else {
       if (!$_SESSION['user']['usuario_esResponsable']) {
@@ -23,20 +24,33 @@
           manejarError('false','Limite alcanzado', "Solo puedes tener una postulacion activa.");
           $stmtCantPostulaciones = null;
           $stmtTransportista = null;
+          $conexion = null;
           exit;
         } 
       }
     } 
+    $stmtAutor = $conexion->query("SELECT usuario_autor FROM publicaciones WHERE publicacion_id = $pubId");
+    if ($stmtAutor->rowCount() == 1) {
+      $stmtAutor = null;
+      $conexion = null;
+      manejarError('false', 'Eres el autor', 'No puedes postularte a tu propia publicacion!');
+      exit;
+    }
 
-    $stmtPostularse = $conexion->prepare('INSERT INTO postulaciones (usuarios_postulante, postulacion_precio, postulacion_descr, publicacion_id) VALUES (?, ?, ?, ?)');
-    $stmtPostularse->bindParam(1, $pubId, PDO::PARAM_INT);
-    $stmtPostularse->bindParam(2, $monto, PDO::PARAM_INT);
+    $stmtPostularse = $conexion->prepare('INSERT INTO postulaciones (usuarios_postulante, postulacion_precio, postulacion_descr, publicacion_id, postulacion_fecha) VALUES (?, ?, ?, ?, ?)');
+    $fechaActual = date('Y-m-d H:i:s');
+    $stmtPostularse->bindParam(1, $transportista, PDO::PARAM_INT);
+    $stmtPostularse->bindParam(2, $monto, PDO::PARAM_STR);
     $stmtPostularse->bindParam(3, $descripcion, PDO::PARAM_STR);
-    $stmtPostularse->bindParam(4, $transportista, PDO::PARAM_STR);
+    $stmtPostularse->bindParam(4, $pubId, PDO::PARAM_INT);
+    $stmtPostularse->bindParam(5, $fechaActual, PDO::PARAM_STR);
     if($stmtPostularse->execute()) {
+      $stmtPostularse = null;
+      $conexion = null;
       manejarError('true', "Postulacion Realizada", 'Postulacion registrada con exito', '../public/index.php?#');
     } else {
       $stmtPostularse = null;
+      $conexion = null;
       manejarError('false',"Error Inesperado", "Ocurrio un error al momento de realizar tu postulacion, intente de nuevo más tarde");
       exit;
     }
