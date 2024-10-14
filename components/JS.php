eef3779eb027a309720e5fa80b1fa8ac793470e0
Lean998 <leandroaguero83@gmail.com>
2024-10-14 19:49:38 -0300

    Include todos los js en el archivo correspondiente

--- components/JS.php
+++ components/JS.php
@@ -2,3 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 <script src="/js/header.js"></script> 
 <script src="/js/ajax.js"></script>
+<script src="/js/formularioPublicaciones.js"></script>
+<script src="/js/inputFotos.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="/js/postulacion.js"></script>
