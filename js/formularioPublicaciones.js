5d85efee1bf69a547ce6eb327763e06e8d3c962d
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-10-19 20:03:31 -0300

    update
    Lo actualice, aunque ya esta en ajax.php, no lo borro por las dudas

--- js/formularioPublicaciones.js
+++ js/formularioPublicaciones.js
@@ -76,28 +76,100 @@ function validarPublicacion() {
     pesoFeedback.textContent = '';
   }
 // Validar Origen y Destino
-  const origen = document.getElementById('publicacion-origen');
-  const destino = document.getElementById('publicacion-destino');
-  const origenFeedback = document.getElementById('invalid-origen')
-  const destinoFeedback =document.getElementById('invalid-destino');
-  if (origen.value.trim() == '') {
-    origenFeedback.textContent = 'El lugar de origen es obligatorio.';
+  const origen_barrio = document.querySelector('#publicacion-origen-barrio');
+  const origen_barrio_Feedback = document.querySelector('#invalid-origen-barrio');
+  if (origen_barrio.value.trim() == '') {
+    origen_barrio_Feedback.textContent = 'El barrio de origen es obligatorio.';
+    origen_barrio.classList.add('is-invalid');
+    isValid = false;
+  } else {
+    origen_barrio.classList.remove('is-invalid');
+    origen_barrio.classList.add('is-valid');
+    origen_barrio_Feedback.textContent = '';
+  }
+  const origen_manzana = document.querySelector('#publicacion-origen-manzana');
+  const origen_manzana_Feedback = document.querySelector('#invalid-origen-manzana');
+  if (origen_manzana.value.trim() == '') {
+    origen_manzana_Feedback.textContent = 'La manzana/piso de origen es obligatorio.';
+    origen_manzana.classList.add('is-invalid');
+    isValid = false;
+  } else {
+    origen_manzana.classList.remove('is-invalid');
+    origen_manzana.classList.add('is-valid');
+    origen_manzana_Feedback.textContent = '';
+  }
+  const origen_casa = document.querySelector('#publicacion-origen-casa');
+  const origen_casa_Feedback = document.querySelector('#invalid-origen-casa');
+  if (origen_casa.value.trim() == '') {
+    origen_casa_Feedback.textContent = 'La casa/depto de origen es obligatorio.';
+    origen_casa.classList.add('is-invalid');
+    isValid = false;
+  } else {
+    origen_casa.classList.remove('is-invalid');
+    origen_casa.classList.add('is-valid');
+    origen_casa_Feedback.textContent = '';
+  }
+  
+  const destino_barrio = document.querySelector('#publicacion-destino-barrio');
+  const destino_barrio_Feedback = document.querySelector('#invalid-destino-barrio');
+  if (destino_barrio.value.trim() == '') {
+    destino_barrio_Feedback.textContent = 'El barrio de destino es obligatorio.';
+    destino_barrio.classList.add('is-invalid');
+    isValid = false;
+  } else {
+    destino_barrio.classList.remove('is-invalid');
+    destino_barrio.classList.add('is-valid');
+    destino_barrio_Feedback.textContent = '';
+  }
+  const destino_manzana = document.querySelector('#publicacion-destino-manzana');
+  const destino_manzana_Feedback = document.querySelector('#invalid-destino-manzana');
+  if (destino_manzana.value.trim() == '') {
+    destino_manzana_Feedback.textContent = 'La manzana/piso de destino es obligatorio.';
+    destino_manzana.classList.add('is-invalid');
+    isValid = false;
+  } else {
+    destino_manzana.classList.remove('is-invalid');
+    destino_manzana.classList.add('is-valid');
+    destino_manzana_Feedback.textContent = '';
+  }
+  const destino_casa = document.querySelector('#publicacion-destino-casa');
+  const destino_casa_Feedback = document.querySelector('#invalid-destino-casa');
+  if (destino_casa.value.trim() == '') {
+    destino_casa_Feedback.textContent = 'La casa/depto de destino es obligatorio.';
+    destino_casa.classList.add('is-invalid');
+    isValid = false;
+  } else {
+    destino_casa.classList.remove('is-invalid');
+    destino_casa.classList.add('is-valid');
+    destino_casa_Feedback.textContent = '';
+  }
+  const mapaFeedback = document.querySelector('#invalid-map');
+  const origen = document.querySelector('#coordsOrigen');
+  const destino = document.querySelector('#coordsDestino');
+  if (origen.value.trim() == '' && destino.value.trim() == '') {
+    mapaFeedback.textContent = 'Seleccione el origen y el destino en el mapa.';
+    mapaFeedback.setAttribute("style","display:flex;flex-direction:column;align-items:center");
     origen.classList.add('is-invalid');
+    destino.classList.add('is-invalid');
+    isValid = false;
+  } else if (origen.value.trim() == '') {
+    mapaFeedback.textContent = 'Seleccione el origen en el mapa.';
+    mapaFeedback.setAttribute("style","display:flex;flex-direction:column;align-items:center");
+    origen.classList.add('is-invalid');
+    isValid = false;
+  } else if (destino.value.trim() == '') {
+    mapaFeedback.textContent = 'Seleccione el destino en el mapa.';
+    mapaFeedback.setAttribute("style","display:flex;flex-direction:column;align-items:center");
+    destino.classList.add('is-invalid');
     isValid = false;
   } else {
     origen.classList.remove('is-invalid');
     origen.classList.add('is-valid');
-    origenFeedback.textContent = '';
-  }
-  if (destino.value.trim() == '') {
-    destinoFeedback.textContent = 'El lugar de destino es obligatorio.';
-    destino.classList.add('is-invalid');
-    isValid = false;
-  } else {
     destino.classList.remove('is-invalid');
     destino.classList.add('is-valid');
-    destinoFeedback.textContent = '';
+    mapaFeedback.textContent = '';
   }
+  
 // Validar persona recibe
   const regexAlfa = /^[a-zA-Z\s]+$/;
   const recibe = document.getElementById('publicacion-recibe');
@@ -137,23 +209,21 @@ function validarPublicacion() {
     contactoFeedback.textContent = '';
   }
 
-  const imagen = document.getElementById('addNewPhoto');
-  const imagenes = imagen.files;
-  const imagenFeedBack = document.getElementById('invalid-photo');
-  const formatosValidos = ['image/png', 'image/jpeg' , 'image/jpg'];
-  if (imagenes.length < 1) {
-    imagenFeedBack.textContent = 'Ingrese al menos una imagen';
-    imagen.classList.add('is-invalid');
-    isValid = false;
-  } else if(imagenes.length > 5){
-    imagenFeedBack.textContent = 'Se permiten como maximo 5 imagenes';
-    imagen.classList.add('is-invalid');
+// Validar Fotos
+  const imagenes = document.querySelector('#photos');
+  const cantImagenes = document.querySelector('#photos').childElementCount;
+  const imagenFeedBack = document.getElementById('invalid-photos');
+  if (cantImagenes < 1) {
+    imagenFeedBack.textContent = 'Ingrese al menos una foto';
+    imagenes.classList.add('is-invalid');
+    imagenFeedBack.setAttribute("style","display:block");
     isValid = false;
   } else{
-    imagen.classList.remove('is-invalid');
-    imagen.classList.add('is-valid');
+    imagenes.classList.remove('is-invalid');
+    imagenes.classList.add('is-valid');
     imagenFeedBack.textContent = "";
   }
+
   return isValid; 
 }
 
@@ -163,13 +233,14 @@ function validarPublicacion() {
     event.preventDefault();
     if (!validarPublicacion()) {
       event.stopPropagation();
-    } 
+    }
+
   }, false);
 })();
 const sendForm = async(action,config)=>{
   return await fetch(action,config)
   .then(
-      respuesta => respuesta.text()
+    respuesta => console.log(respuesta.json())
   )
   .then(
       response=>console.log(response))
