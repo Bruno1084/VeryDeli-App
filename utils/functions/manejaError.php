aec64d2c92dcf36a70d331996b005b37d1613bd3
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-10-21 15:40:52 -0300

    ahora puede redireccionar en los errores

--- utils/functions/manejaError.php
+++ utils/functions/manejaError.php
@@ -1,7 +1,13 @@
 <?php
 function manejarError($estado, $titulo, $mensaje, $redirect = "") {
   if(($estado == "false")){
-    echo json_encode(['success' => $estado, 'message' => '<div class="text-bg-danger p-3"><strong>¡' . $titulo . '!</strong><br>' . $mensaje . '</div>']);
+    if($redirect == ""){
+      echo json_encode(['success' => $estado, 'message' => '<div class="text-bg-danger p-3"><strong>¡' . $titulo . '!</strong><br>' . $mensaje . '</div>']);
+    }
+    else{
+      echo json_encode(['success' => $estado, 'message' => '<div class="text-bg-danger p-3"><strong>¡' . $titulo . '!</strong><br>' . $mensaje . '</div>', 'redirect' => $redirect]);
+
+    }
   } else {
     if($redirect == ""){
       echo json_encode(['success' => $estado, 'message' => '<div class="text-bg-success p-3"><strong>¡' . $titulo . '!</strong><br>'.$mensaje . '</div>']);
