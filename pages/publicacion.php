c797cdd7d8fd1d368258711bed667e81129a329d
Lean998 <leandroaguero83@gmail.com>
2024-10-23 13:26:05 -0300

    add js y componentes necesarios para su funcionamiento

--- pages/publicacion.php
+++ pages/publicacion.php
@@ -7,16 +7,20 @@
   <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/components/publicacionExtendida.php");?>
   <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/database/conection.php");?>
   <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/utils/get/getPublicacion.php");?>
+  <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/components/JS.php');?>
   <title>Very Deli</title>
 </head>
 <body>
   <?php require_once("../components/Header.php");?>
+  <div class="d-flex justify-content-center">
+    <div class="form-rest my-4 col-8"></div>
+  </div>
   <?php 
     $publicacion = getPublicacion($_GET['id']);
 
     $imagenes = json_decode($publicacion['imagenes'], true);
 
-
+    
     echo renderPublicacionExtendida(
       $publicacion['publicacion_id'],
       $publicacion['usuario_usuario'],
@@ -31,6 +35,10 @@
     );
   ?>
 
-  <?php require_once("../components/Footer.php");?>  
+  <?php require_once("../components/Footer.php");?>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="/js/postulacion.js"></script>
+  <script src="/js/ajax.js"></script>
+  <script src="/js/validarReporte.js"></script>
 </body>
 </html>
