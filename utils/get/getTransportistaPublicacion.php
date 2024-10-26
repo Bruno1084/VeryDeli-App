6b5950db6e52ad083f1057d559696b0ec62a6f7a
Lean998 <leandroaguero83@gmail.com>
2024-10-26 13:52:07 -0300

    obtener la postulación que fue aceptada

---
+++ utils/get/getTransportistaPublicacion.php
@@ -0,0 +1,24 @@
+<?php
+function getTransportistaPublicacion ($idPublicacion) {
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/database/conection.php");
+    $DB = new DB();
+    $conexion = $DB->getConnection();
+
+    $sql = "SELECT publicaciones.*, postulaciones.*
+            FROM publicaciones
+            JOIN postulaciones ON postulaciones.usuario_postulante = publicaciones.usuario_transportista
+            WHERE publicaciones.publicacion_id = ? 
+            AND publicaciones.usuario_transportista IS NOT NULL
+            AND postulaciones.postulacion_estado = 2 ";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bindValue(1, $idPublicacion, PDO::PARAM_INT);
+    $stmt->execute();
+  
+    $postulacion = $stmt->fetch(PDO::FETCH_ASSOC);
+  
+    $DB = null;
+    $stmt = null;
+    $conexion = null;
+  
+    return $postulacion;
+  }
