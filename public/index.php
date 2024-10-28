0318ffd6881b9ad1211a348e42a59d7ad8c43485
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-10-28 19:25:48 -0300

    update iniciar sesion obligatorio

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/utils/functions/startSession.php");
 
 if(empty($_SESSION)){
+  session_unset();
+  session_destroy();
+  setcookie("VERY-SESSION", "", time() - 3600, "/");
   require_once($_SERVER["DOCUMENT_ROOT"]."/components/login.php");
 }
 else{
