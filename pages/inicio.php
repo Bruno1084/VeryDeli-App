fdaaffa8bbff04a11b994397529ef3df7c3e6447
Bruno Sosa <sosabruno3384@gmail.com>
2024-10-30 19:35:05 -0300

    tratando de arreglar el dropdown del header

--- pages/inicio.php
+++ pages/inicio.php
@@ -9,17 +9,12 @@
   <title>Very Deli</title>
 </head>
 <body>
-  <?php require_once("../components/Header.php");?>
+  <?php require_once($_SERVER['DOCUMENT_ROOT']."/components/Header.php");?>
   <?php require_once($_SERVER ['DOCUMENT_ROOT'].'/components/nuevaPublicacion.php') ?>
   <?php require_once($_SERVER["DOCUMENT_ROOT"]."/components/publicaciones.php")?>
   <?php require_once($_SERVER["DOCUMENT_ROOT"]."/utils/get/getAllUsuarios.php")?>
-  <div>
-    <?php
-    if(isset($_SESSION['id'])){
-      echo('Bienvenido '.$_SESSION['id'].'!');
-    }
-    ?>
 
+  <div>
     <div class="container container-fluid d-flex justify-content-center">
       <div class="form-rest my-4 col-8">
         <div class="text-bg-secondary d-flex justify-content-between p-3">
