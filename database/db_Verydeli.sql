49566aa52dba4cd74efb946ae122482c47c2aa49
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-10-31 22:55:14 -0300

    detalles

--- database/db_Verydeli.sql
+++ database/db_Verydeli.sql
@@ -37,7 +37,7 @@ CREATE TABLE `vehiculos` (
 CREATE TABLE `publicaciones` (
     `publicacion_id` int AUTO_INCREMENT NOT NULL ,
     `publicacion_titulo` varchar(500)  NOT NULL ,
-    `publicacion_fecha` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `publicacion_fecha` DATETIME DEFAULT ,
     `publicacion_descr` varchar(500)  NOT NULL ,
     `publicacion_volumen` float  NULL ,
     `publicacion_peso` float  NULL ,
@@ -80,7 +80,7 @@ CREATE TABLE `comentarios` (
     `publicacion_id` int  NOT NULL ,
     `usuario_id` int  NOT NULL ,
     `comentario_mensaje` varchar(500)  NOT NULL ,
-    `comentario_fecha` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `comentario_fecha` DATETIME DEFAULT ,
     `comentario_esActivo` tinyint(1) NOT NULL,
     PRIMARY KEY (
         `comentario_id`
@@ -93,7 +93,7 @@ CREATE TABLE `postulaciones` (
     `usuario_postulante` int  NOT NULL ,
     `postulacion_precio` float  NOT NULL ,
     `postulacion_descr` varchar(500)  NULL ,
-    `postulacion_fecha` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `postulacion_fecha` DATETIME DEFAULT ,
     `postulacion_estado` enum('0', '1', '2') NOT NULL DEFAULT '0'
     PRIMARY KEY (
         `postulacion_id`
@@ -106,7 +106,7 @@ CREATE TABLE `calificaciones` (
     `usuario_calificado` int  NOT NULL ,
     `usuario_calificador` int  NOT NULL ,
     `calificacion_puntaje` enum('1','2','3','4','5')  NOT NULL ,
-    `calificacion_fecha` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `calificacion_fecha` DATETIME DEFAULT ,
     PRIMARY KEY (
         `calificacion_id`
     )
@@ -139,6 +139,7 @@ CREATE TABLE `notificaciones` (
     `notificacion_id` INT NOT NULL AUTO_INCREMENT ,
     `notificacion_mensaje` VARCHAR(255) NOT NULL ,
     `notificacion_estado` BOOLEAN NOT NULL ,
+    `notificacion_fecha` DATETIME DEFAULT,
     `usuario_id` INT NOT NULL ,
     `publicacion_id` INT NULL,
     PRIMARY KEY (
@@ -151,7 +152,7 @@ CREATE TABLE `publicaciones_reportadas` (
     `usuario_autor` int  NOT NULL ,
     `reporte_motivo` varchar(30)  NOT NULL ,
     `reporte_mensaje` varchar(255) NULL ,
-    `reporte_fecha` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `reporte_fecha` DATETIME DEFAULT ,
     `reporte_activo` tinyint(1) DEFAULT 1 NOT NULL,
     PRIMARY KEY (
         `reporte_id`
