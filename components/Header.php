81f1cdb35fabf1f689321961380d4a78dae0c6ed
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-11-02 18:56:20 -0300

    detalles distribución espacios header

--- components/Header.php
+++ components/Header.php
@@ -3,7 +3,7 @@
     <div class="content-fluid">
         <div class="col-12">
             <nav class="navbar navbar-expand-lg">
-                <div class="col-auto logoNav">
+                <div class="col-1 logoNav">
                     <a class="navbar-brand" href="../public/index.php">
                         <img src="/assets/Logo.png" class="img-fluid" alt="logo">
                     </a>
@@ -26,51 +26,53 @@
                         </form>
                     </div>
                 </div>
-                <div class="col-auto notifications">
-                    <?php
-                        function idIsNull($pubId){
-                            if($pubId==null){
-                                return "#";
+                <div class="col-1 d-flex notYprof">
+                    <div class="col-auto notifications">
+                        <?php
+                            function idIsNull($pubId){
+                                if($pubId==null){
+                                    return "#";
+                                }
+                                else{
+                                    return "../pages/publicacion.php?id=".$pubId;
+                                }
                             }
-                            else{
-                                return "../pages/publicacion.php?id=".$pubId;
-                            }
-                        }
-                        require_once($_SERVER["DOCUMENT_ROOT"]."/utils/get/getAllNotificacionesNoVistasFromUsuario.php"); 
-                        $notificaciones=getNotificacionesActivasFromUsuario(5); 
-                    ?>
-                    <div class="dropdown">
-                            <button class="btn dropdown-toggle py-2 px-0 px-lg-2 d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
-                                <img src="/assets/bell.png" class="img-fluid" alt="notify">
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <?php 
-                                    if(sizeof($notificaciones)>0){
-                                        foreach($notificaciones as $notify){?>
-                                            <a class="dropdown-item" href=<?php echo idIsNull($notify["publicacion_id"])?>><?php echo $notify["notificacion_mensaje"] ?></a>
+                            require_once($_SERVER["DOCUMENT_ROOT"]."/utils/get/getAllNotificacionesNoVistasFromUsuario.php"); 
+                            $notificaciones=getNotificacionesActivasFromUsuario(5); 
+                        ?>
+                        <div class="dropdown">
+                                <button class="btn dropdown-toggle py-2 px-0 px-lg-2 d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
+                                    <img src="/assets/bell.png" class="img-fluid" alt="notify">
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <?php 
+                                        if(sizeof($notificaciones)>0){
+                                            foreach($notificaciones as $notify){?>
+                                                <a class="dropdown-item" href=<?php echo idIsNull($notify["publicacion_id"])?>><?php echo $notify["notificacion_mensaje"] ?></a>
+                                                <hr class="dropdown-divider">
+                                                <a class="dropdown-item" href="../components/Notificaciones.php">Ver mas</a>
+                                    <?php       }
+                                        }
+                                        else{
+                                    ?>
+                                            <p class="dropdown-item">Nada por aqui</p>
                                             <hr class="dropdown-divider">
-                                            <a class="dropdown-item" href="../components/Notificaciones.php">Ver mas</a>
-                                <?php       }
-                                    }
-                                    else{
-                                ?>
-                                        <p class="dropdown-item">Nada por aqui</p>
-                                        <hr class="dropdown-divider">
-                                        <a class="dropdown-item" href="../components/Notificaciones.php">Ver Todo</a>
-                                <?php
-                                    }
-                                ?>
-                            </div>
+                                            <a class="dropdown-item" href="../components/Notificaciones.php">Ver Todo</a>
+                                    <?php
+                                        }
+                                    ?>
+                                </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-auto perfilNav">
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="/assets/user.png" class="img-fluid" alt="account">
-                        </button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="../public/miPerfil.php">Mi Perfil</a>
-                            <a class="dropdown-item" href="../utils/functions/cerrarSesion.php">Cerrar Sesion</a>
+                    <div class="col-auto perfilNav">
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="/assets/user.png" class="img-fluid" alt="account">
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="../public/miPerfil.php">Mi Perfil</a>
+                                <a class="dropdown-item" href="../utils/functions/cerrarSesion.php">Cerrar Sesion</a>
+                            </div>
                         </div>
                     </div>
                 </div>
