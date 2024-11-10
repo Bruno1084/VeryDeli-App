84bcedf57ef9ea00008187768799c0d2863a8cbe
Lean998 <leandroaguero83@gmail.com>
2024-11-10 18:50:22 -0300

    add getPostRatingUsuario.php
    Esta función retorna las calificaciones hechas por un usuario a una publicación en especifico.

---
+++ utils/get/getPostRatingUsuario.php
@@ -0,0 +1,39 @@
+<?php
+function getPostRatingUsuario ($idPublicacion, $idUsuario) {
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/database/conection.php");
+
+  $DB = new DB();
+  $conexion = $DB->getConnection();
+
+  $sql = "SELECT 
+    calificaciones.calificacion_id,
+    calificaciones.calificacion_puntaje,
+    calificaciones.calificacion_fecha,
+    calificaciones.usuario_calificador,
+    calificaciones.usuario_calificado,
+    usuario_calificador.usuario_usuario AS nombre_calificador,
+    usuario_calificado.usuario_usuario AS nombre_calificado
+    FROM 
+    publicaciones
+    JOIN 
+    calificaciones ON publicaciones.publicacion_id = calificaciones.publicacion_id
+    JOIN 
+    usuarios AS usuario_calificador ON calificaciones.usuario_calificador = usuario_calificador.usuario_id
+    JOIN 
+    usuarios AS usuario_calificado ON calificaciones.usuario_calificado = usuario_calificado.usuario_id
+    WHERE 
+    publicaciones.publicacion_id = ? AND calificaciones.usuario_calificador = ?;
+      ";
+  $stmt = $conexion->prepare($sql);
+  $stmt->bindValue(1, $idPublicacion, PDO::PARAM_INT);
+  $stmt->bindValue(2, $idUsuario, PDO::PARAM_INT);
+  $stmt->execute();  
+
+  $calificacion = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  $DB = null;
+  $stmt = null;
+  $conexion = null;
+
+  return $calificacion;
+}
