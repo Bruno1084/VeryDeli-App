3dc44063b7ba5de3a69361cc1bdbd00fe716be00
Lean998 <leandroaguero83@gmail.com>
2024-11-10 18:13:14 -0300

    obtener calificaciones

--- utils/get/getCalificacionesFromPublicacion.php
+++ utils/get/getCalificacionesFromPublicacion.php
@@ -6,20 +6,23 @@ function getCalificacionesFromPublicacion ($idPublicacion) {
   $conexion = $DB->getConnection();
 
   $sql = "SELECT 
-      calificaciones.calificacion_id,
-      calificaciones.calificacion_puntaje,
-      calificaciones.calificacion_fecha,
-      calificaciones.usuario_calificador,
-      calificaciones.usuario_calificado,
-      usuarios.usuario_nombre AS usuario_nombre
-      FROM 
-      publicaciones
-      JOIN 
-      calificaciones ON publicaciones.publicacion_id = calificaciones.publicacion_id
-      JOIN 
-      usuarios ON calificaciones.usuario_calificado = usuarios.usuario_id
-      WHERE 
-      publicaciones.publicacion_id = ?;
+    calificaciones.calificacion_id,
+    calificaciones.calificacion_puntaje,
+    calificaciones.calificacion_fecha,
+    calificaciones.usuario_calificador,
+    calificaciones.usuario_calificado,
+    usuario_calificador.usuario_usuario AS nombre_calificador,
+    usuario_calificado.usuario_usuario AS nombre_calificado
+    FROM 
+    publicaciones
+    JOIN 
+    calificaciones ON publicaciones.publicacion_id = calificaciones.publicacion_id
+    JOIN 
+    usuarios AS usuario_calificador ON calificaciones.usuario_calificador = usuario_calificador.usuario_id
+    JOIN 
+    usuarios AS usuario_calificado ON calificaciones.usuario_calificado = usuario_calificado.usuario_id
+    WHERE 
+    publicaciones.publicacion_id = ?;
       ";
   $stmt = $conexion->prepare($sql);
   $stmt->bindValue(1, $idPublicacion, PDO::PARAM_INT);
