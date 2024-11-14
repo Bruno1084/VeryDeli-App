8f887652781c8cb39ca987a877f8ede96f63c904
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-11-14 18:54:04 -0300

    filtro denuncias

--- utils/get/getAllDenunciasPendientes.php
+++ utils/get/getAllDenunciasPendientes.php
@@ -5,10 +5,16 @@ function getAllDenunciasPendientes($limit = 0, $offset = 0){
 $db=new DB();
 $conexion=$db->getConnection();
 
-$sql="  SELECT *
+$sql="  SELECT denuncias_reportadas.*
         FROM denuncias_reportadas
-        WHERE reporte_activo='1'
-        ORDER BY reporte_fecha ASC
+        LEFT JOIN publicaciones ON publicaciones.publicacion_id=denuncias_reportadas.publicacion_id
+        AND publicaciones.usuario_autor=?
+        LEFT JOIN comentarios ON comentarios.comentario_id=denuncias_reportadas.comentario_id
+        AND comentarios.usuario_id=?
+        WHERE denuncias_reportadas.reporte_activo='1'
+        AND publicaciones.usuario_autor IS NULL
+        AND comentarios.usuario_id IS NULL
+        ORDER BY denuncias_reportadas.reporte_fecha ASC
     ";
 
 if ($limit > 0) {
@@ -21,11 +27,13 @@ if($offset > 0){
 
 $stmt=$conexion->prepare($sql);
 
+$stmt->bindValue(1, $_SESSION["id"], PDO::PARAM_INT);
+$stmt->bindValue(2, $_SESSION["id"], PDO::PARAM_INT);
 if ($limit > 0) {
-    $stmt->bindValue(1, $limit, PDO::PARAM_INT);
+    $stmt->bindValue(3, $limit, PDO::PARAM_INT);
 };
 if ($offset > 0){
-    $stmt->bindValue(2, $offset, PDO::PARAM_INT);
+    $stmt->bindValue(4, $offset, PDO::PARAM_INT);
 };
 $res=$stmt->execute();
 if($res!=false){
