3125effef5ff198c39150a0f691362e560e9b96e
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-11-16 19:01:24 -0300

    update postudaciones from user

--- utils/get/getAllPostulacionFromUsuario.php
+++ utils/get/getAllPostulacionFromUsuario.php
@@ -5,7 +5,12 @@ function getAllPostulacionFromUsuario ($idUsuario) {
   $DB = new DB();
   $conexion = $DB->getConnection();
 
-  $sql = "SELECT postulacion_fecha, postulacion_estado, publicacion_id FROM postulaciones WHERE ? = postulaciones.usuario_postulante";
+  $sql = "SELECT postulaciones.postulacion_fecha, postulaciones.postulacion_estado, postulaciones.publicacion_id 
+          FROM postulaciones
+          LEFT JOIN denuncias_reportadas ON denuncias_reportadas.publicacion_id=postulaciones.publicacion_id
+          WHERE ? = postulaciones.usuario_postulante
+                AND (denuncias_reportadas.publicacion_id IS NULL OR denuncias_reportadas.reporte_activo='3')
+          ";
   $stmt = $conexion->prepare($sql);
   $stmt->bindValue(1, $idUsuario, PDO::PARAM_INT);
   $stmt->execute();
