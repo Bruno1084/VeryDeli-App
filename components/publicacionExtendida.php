02cb11927ef346e42d7c683abe51573e084f9e94
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-11-16 18:29:41 -0300

    detalles al cargar una publicacion
    No tocar, plis

--- components/publicacionExtendida.php
+++ components/publicacionExtendida.php
@@ -127,12 +127,9 @@ function renderPublicacionExtendida ($idPublicacion, $username, $profileIcon, $d
         <?php }
           }
           elseif($denunciada==1){?>
-          <button type="button" class="btn procesarDenuncia btn-outline-success btn-md" data-name="publicacion" data-id="<?php echo $idPublicacion ?>" onclick="procesarDenuncia(event)">
-            Permitir
-          </button>
-          <button type="button" class="btn procesarDenuncia btn-outline-danger btn-md" data-name="publicacion" data-id="<?php echo $idPublicacion ?>" onclick="procesarDenuncia(event)">
-            Eliminar
-          </button>
+          <button type="button" class="btn procesarDenuncia btn-outline-success btn-md" data-name="publicacion" data-id="<?php echo $idPublicacion ?>" onclick="procesarDenuncia(event)">Permitir</button>
+          <div class="resultDenuncia"><p></p></div>
+          <button type="button" class="btn procesarDenuncia btn-outline-danger btn-md" data-name="publicacion" data-id="<?php echo $idPublicacion ?>" onclick="procesarDenuncia(event)">Eliminar</button>
           <?php }
       ?>
       </div>
