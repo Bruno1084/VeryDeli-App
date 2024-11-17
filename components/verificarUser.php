4f4ad5c305da50fa77f6f83546b89d4d1cf09847
Octavio Riccardo <octariccardo@gmail.com>
2024-11-17 16:26:54 -0300

    Modificar el estado de las verificaciones en la base de datos

--- components/verificarUser.php
+++ components/verificarUser.php
@@ -22,47 +22,50 @@ function renderVerificacion(
     <div class="container-fluid">
         <div class="row">
             <div class="col-3">
-                
-                <div class="VerificacionCard">
-                    <!-- Tarjeta Verificacion -->
-                    <div class="profile-section">
-                        <?php 
-                            if($fotoperfil==false){
-                               echo "<img src='/assets/user.png' alt='Foto de perfil' class='profile-image'>";
-        
-                            }
-                            else{
-                                echo"<img src='$fotoperfil' alt='Foto de perfil' class='profile-image'>";
-                            }
-                        ?>
-                        <span class="user-name"><?php echo "Usuario:".$usuario["usuario_nombre"]; ?></span>
-                        <?php   
-                                if($estado==0){
-                                echo "<h6>Estado:Sin Verificar</h6>";    
-                                }   
-                        ?> 
-                        <spam><?php echo "Localidad:".$usuario["usuario_localidad"];?></spam>
-                    </div>
-                    <!-- Foto DNI del usuario -->
-                    <div class="id-section">
-                        <?php 
-                            if(($fotodoc1 || $fotodoc2)!=null){
-                                echo "<img src='$fotodoc1' alt='Foto DNI' class='id-image'>";
-                                echo "<img src='$fotodoc2' alt='Foto DNI' class='id-image'>";
-                            }
-                            elseif(($fotoboleta1 || $fotoboleta2)!=null){
-                                echo "<img src='$fotoboleta1' alt='Foto DNI' class='id-image'>";
-                                echo "<img src='$fotoboleta2' alt='Foto DNI' class='id-image'>";
-                            }
-                        
-                        ?>
-                        
-                        <div class="button-section">
-                            <button class="btn btn-accept">Aceptar</button>
-                            <button class="btn btn-reject">Rechazar</button>
+               <form>
+                    <div class="VerificacionCard">
+                        <!-- Tarjeta Verificacion -->
+                        <div class="profile-section">
+                            <?php 
+                                if($fotoperfil==false){
+                                echo "<img src='/assets/user.png' alt='Foto de perfil' class='profile-image'>";
+            
+                                }
+                                else{
+                                    echo"<img src='$fotoperfil' alt='Foto de perfil' class='profile-image'>";
+                                }
+                            ?>
+                            <span class="user-name"><?php echo "Usuario:".$usuario["usuario_nombre"]; ?></span>
+                            <?php   
+                                    if($estado==0){
+                                    echo "<h6>Estado:Sin Verificar</h6>";    
+                                    }   
+                            ?> 
+                            <spam><?php echo "Localidad:".$usuario["usuario_localidad"];?></spam>
+                    
+                            <spam><?php echo "Tipo de Documento:".$tipodoc?></spam>
+                            <spam><?php echo "Tipo de Documento:".$tipoboleta?></spam>
+                        </div>
+                        <!-- Foto DNI del usuario -->
+                        <div class="id-section">
+                            <?php 
+                                if(($fotodoc1 || $fotodoc2)!=null){
+                                    echo "<img src='$fotodoc1' alt='Foto DNI' class='id-image'>";
+                                    echo "<img src='$fotodoc2' alt='Foto DNI' class='id-image'>";
+                                }
+                                elseif(($fotoboleta1 || $fotoboleta2)!=null){
+                                    echo "<img src='$fotoboleta1' alt='Foto DNI' class='id-image'>";
+                                    echo "<img src='$fotoboleta2' alt='Foto DNI' class='id-image'>";
+                                }
+                            ?>
+                            <div class="button-section">
+                                <!-- 0="pendiente", a 1="aceptado" o 2="rechazado" -->
+                                <button type="button" class="btn btn-accept" title="Aceptar" data-estadp="<?= $estado=1 ?>" data-id="<?= $verificacionid ?>" onclick="cambiarEstadoVerificacion(this)">Aceptar</button>
+                                <button type="button" class="btn btn-reject" title="Rechazar" data-estadp="<?= $estado=2 ?>" data-id="<?= $verificacionid ?>" onclick="cambiarEstadoVerificacion(this)">Rechazar</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form> 
             </div>
         </div>
     </div>    
--- pages/verificaciones.php
+++ pages/verificaciones.php
@@ -6,6 +6,7 @@
   <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/components/head.php");?>
   <link rel="stylesheet" href="../css/verificacion.css">
   <link rel="stylesheet" href="/css/miPerfil.css">
+   <script src="/js/cambiarEstadoVerificacion.js"></script>
   <?php 
 
   include_once($_SERVER['DOCUMENT_ROOT'] . "/database/conection.php");
@@ -21,26 +22,30 @@
   require_once("../components/Header.php");
   $verificaciones=getAllVerificaciones();
   ?>
-  <section class="col-12 cuerpo">
-  <aside>
-    <?php
-      foreach ($verificaciones as $verificacion) {
-        
-         echo renderVerificacion(
-          $verificacion["verificacion_id"],
-          $verificacion["verificacion_foto-doc1"],
-          $verificacion["verificacion_foto-doc2"],
-          $verificacion["verificacion_foto-boleta1"],
-          $verificacion["verificacion_foto-boleta2"],
-          $verificacion["verificacion_tipo-doc"],
-          $verificacion["verificacion_tipo-boleta"],
-          $verificacion["verificacion_estado"],
-          $verificacion["usuario_id"]
-        ); 
-      }
-    ?>
-    </aside>
-  </section>
+  <div class="container-fluid cuerpo">
+    <div class="row">
+      <h1 class="d-flex justify-content-center">Verificaciones Pendientes</h1>
+      <div class="col-12 d-flex justify-content-center">
+          
+        <?php
+          foreach ($verificaciones as $verificacion) {
+            
+            echo renderVerificacion(
+              $verificacion["verificacion_id"],
+              $verificacion["verificacion_foto-doc1"],
+              $verificacion["verificacion_foto-doc2"],
+              $verificacion["verificacion_foto-boleta1"],
+              $verificacion["verificacion_foto-boleta2"],
+              $verificacion["verificacion_tipo-doc"],
+              $verificacion["verificacion_tipo-boleta"],
+              $verificacion["verificacion_estado"],
+              $verificacion["usuario_id"]
+            ); 
+          }
+        ?> 
+      </div>
+    </div>
+  </div>
   <?php require_once("../components/Footer.php");?>
   <?php require_once($_SERVER["DOCUMENT_ROOT"]."/components/JS.php")?>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
