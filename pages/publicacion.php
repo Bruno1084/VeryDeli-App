3dfff0f45ee5f059e7485016d774da4b7ae90923
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2024-12-07 20:17:01 -0300

    carga completa de datos publicacion
    Muestra todo lo pedido al crear la publicacion, solo al autor, transportista, y a los admins (en caso de que haya sido denunciada)

--- pages/publicacion.php
+++ pages/publicacion.php
@@ -5,20 +5,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/components/head.php"); ?>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
   <link rel="stylesheet" href="/css/publicacionExtendida.css">
   <?php
   include_once($_SERVER['DOCUMENT_ROOT'] . "/components/publicacionExtendida.php");
   include_once($_SERVER['DOCUMENT_ROOT'] . "/database/conection.php");
   include_once($_SERVER['DOCUMENT_ROOT'] . "/utils/get/getPublicacion.php");
   include_once($_SERVER['DOCUMENT_ROOT'] . "/utils/get/getAutorPublicacion.php");
+  require_once($_SERVER['DOCUMENT_ROOT'] . "/utils/get/getAllAdmins.php");
   require_once($_SERVER['DOCUMENT_ROOT'] . '/components/listaPostulaciones.php');
   require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/functions/startSession.php');
-
   ?>
    <script src="/js/cambiarEstado.js"></script>
   <title>Very Deli</title>
 </head>
-
 <body>
   <?php require_once("../components/Header.php"); ?>
   <div class="d-flex justify-content-center">
@@ -28,86 +28,132 @@
     <?php
     $publicacion = getPublicacion($_GET['id']);
     if ($publicacion != false) {
-
+      $admins=null;
       $denuncia=null;
       if(isset($_GET["denuncia"])){
+        $admins=getAllAdmins();
         $denuncia=$_GET["denuncia"];
-      }
-      $autor = getAutorPublicacion($_GET['id']);
-      $imagenes = json_decode($publicacion['imagenes']);
-
-      $ubicaciones = json_decode($publicacion["ubicaciones"]);
-      
-      if($publicacion['publicacion_esActivo'] == "3"){
-        require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/get/getCalificacionesFromPublicacion.php');
-        require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/functions/funcionesCalificaciones.php');
-        if ($_SESSION['id'] == $publicacion['usuario_transportista']) {
-          $calificaciones = getCalificacionesFromPublicacion($_GET['id']);
-          if(!empty($calificaciones)){
-            $calificacionTransportista = [];
-            if(count($calificaciones) == 2){
-              renderCalificaciones($calificaciones);
-            } else {
-              foreach($calificaciones as $calificacion){
-                if($calificacion['usuario_calificado'] == $_SESSION['id']){
-                  $calificacionTransportista[] = $calificacion;
+        if(in_array($_SESSION["id"],$admins)){
+          $autor = getAutorPublicacion($_GET['id']);
+          $imagenes = json_decode($publicacion['imagenes']);
+          $ubicaciones = json_decode($publicacion["ubicaciones"]);
+          if($publicacion['publicacion_esActivo'] == "3"){
+            require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/get/getCalificacionesFromPublicacion.php');
+            require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/functions/funcionesCalificaciones.php');
+            if ($_SESSION['id'] == $publicacion['usuario_transportista']) {
+              $calificaciones = getCalificacionesFromPublicacion($_GET['id']);
+              if(!empty($calificaciones)){
+                $calificacionTransportista = [];
+                if(count($calificaciones) == 2){
+                  renderCalificaciones($calificaciones);
+                } else {
+                  foreach($calificaciones as $calificacion){
+                    if($calificacion['usuario_calificado'] == $_SESSION['id']){
+                      $calificacionTransportista[] = $calificacion;
+                    }
+                  }
+                  if(!empty($calificacionTransportista)) {
+                    renderCalificaciones($calificacionTransportista);
+                  }
                 }
-              }
-              if(!empty($calificacionTransportista)) {
-                renderCalificaciones($calificacionTransportista);
-              }
+              } 
             }
-            
-            
-          } 
+          }
+          // Renderiza postulaciones solo si la publicación le pertenece al usuario 
+          if($_SESSION['id'] == $autor['usuario_autor']){
+            echo renderPostulaciones($_GET['id']);
+          }
+          $foto=array("foto"=>$publicacion["usuario_fotoPerfil"],"marco"=>$publicacion["usuario_marcoFoto"]);
+          echo renderPublicacionExtendida(
+            $publicacion['publicacion_id'],
+            $publicacion["usuario_id"],
+            $publicacion['usuario_usuario'],
+            $foto,
+            $publicacion['publicacion_fecha'],
+            $publicacion['usuario_localidad'],
+            $publicacion['publicacion_titulo'],
+            $publicacion['publicacion_descr'],
+            $publicacion['publicacion_peso'],
+            $publicacion['publicacion_volumen'],
+            $publicacion["publicacion_nombreRecibe"],
+            $publicacion["publicacion_telefono"],
+            $ubicaciones->origen,
+            $ubicaciones->destino,
+            $imagenes,
+            $publicacion["publicacion_esActivo"],
+            $denuncia
+          );
         }
       }
-      
-      // Renderiza postulaciones solo si la publicación le pertenece al usuario 
-      if($_SESSION['id'] == $autor['usuario_autor']){
-        echo renderPostulaciones($_GET['id']);
-      }
-      
-      $foto=array("foto"=>$publicacion["usuario_fotoPerfil"],"marco"=>$publicacion["usuario_marcoFoto"]);
-      if($denuncia!=null){
-        echo renderPublicacionExtendida(
-          $publicacion['publicacion_id'],
-          $autor,
-          $publicacion['usuario_usuario'],
-          $foto,
-          $publicacion['publicacion_fecha'],
-          $publicacion['usuario_localidad'],
-          $publicacion['publicacion_descr'],
-          $publicacion['publicacion_peso'],
-          $ubicaciones->origen->barrio,
-          $ubicaciones->destino->barrio,
-          $imagenes,
-          $publicacion["publicacion_esActivo"],
-          $denuncia
-        );
-      }
       else{
-        echo renderPublicacionExtendida(
-          $publicacion['publicacion_id'],
-          $publicacion['usuario_id'],
-          $publicacion['usuario_usuario'],
-          $foto,
-          $publicacion['publicacion_fecha'],
-          $publicacion['usuario_localidad'],
-          $publicacion["publicacion_titulo"],
-          $publicacion['publicacion_descr'],
-          $publicacion['publicacion_peso'],
-          $ubicaciones->origen->barrio,
-          $ubicaciones->destino->barrio,
-          $imagenes,
-          $publicacion["publicacion_esActivo"]
-        );
+        $tmpDB=new DB();
+        $tmpConexion=$tmpDB->getConnection();
+        $tmpSql="SELECT publicacion_id FROM denuncias_reportadas WHERE publicacion_id = ? AND (reporte_activo = 1 OR reporte_activo = 2)";
+        $tmpStmt=$tmpConexion->prepare($tmpSql);
+        $tmpStmt->bindParam(1,$publicacion,PDO::PARAM_INT);
+        $res=false;
+        if($tmpStmt->execute()){
+          $res=$tmpStmt->fetch(PDO::FETCH_ASSOC);
+        }
+        $tmpDB=null;
+        $tmpConexion=null;
+        $tmpSql=null;
+        $tmpStmt=null;
+        if($res==false){
+          $autor = getAutorPublicacion($_GET['id']);
+          $imagenes = json_decode($publicacion['imagenes']);
+          $ubicaciones = json_decode($publicacion["ubicaciones"]);
+          if($publicacion['publicacion_esActivo'] == "3"){
+            require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/get/getCalificacionesFromPublicacion.php');
+            require_once($_SERVER['DOCUMENT_ROOT'] . '/utils/functions/funcionesCalificaciones.php');
+            if ($_SESSION['id'] == $publicacion['usuario_transportista']) {
+              $calificaciones = getCalificacionesFromPublicacion($_GET['id']);
+              if(!empty($calificaciones)){
+                $calificacionTransportista = [];
+                if(count($calificaciones) == 2){
+                  renderCalificaciones($calificaciones);
+                } else {
+                  foreach($calificaciones as $calificacion){
+                    if($calificacion['usuario_calificado'] == $_SESSION['id']){
+                      $calificacionTransportista[] = $calificacion;
+                    }
+                  }
+                  if(!empty($calificacionTransportista)) {
+                    renderCalificaciones($calificacionTransportista);
+                  }
+                }
+              } 
+            }
+          }
+          // Renderiza postulaciones solo si la publicación le pertenece al usuario 
+          if($_SESSION['id'] == $autor['usuario_autor']){
+            echo renderPostulaciones($_GET['id']);
+          }
+          $foto=array("foto"=>$publicacion["usuario_fotoPerfil"],"marco"=>$publicacion["usuario_marcoFoto"]);
+          echo renderPublicacionExtendida(
+            $publicacion['publicacion_id'],
+            $publicacion['usuario_id'],
+            $publicacion['usuario_usuario'],
+            $foto,
+            $publicacion['publicacion_fecha'],
+            $publicacion['usuario_localidad'],
+            $publicacion["publicacion_titulo"],
+            $publicacion['publicacion_descr'],
+            $publicacion['publicacion_peso'],
+            $publicacion['publicacion_volumen'],
+            $publicacion["publicacion_nombreRecibe"],
+            $publicacion["publicacion_telefono"],
+            $ubicaciones->origen,
+            $ubicaciones->destino,
+            $imagenes,
+            $publicacion["publicacion_esActivo"]
+          );
+        }
       }
     }
     ?>
   </div>
   <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/Footer.php");?>
-  
   <?php require_once($_SERVER["DOCUMENT_ROOT"]."/components/JS.php")?>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="/js/postulacion.js"></script>
@@ -122,9 +168,6 @@
   <?php }
   else{?>
     <script src="/js/procesarDenuncia.js"></script>
-<?php
-  }
-  ?>
+<?php }?>
 </body>
-
 </html>
